cfcda55bb65e47aed3b211df909a7c10a4580836
gabboas <gmaia@outlook.ie>
2024-02-07 01:46:42 +0000

    Buttons shaped, js and some layout added.

---
+++ src/js/navbuttons.js
@@ -0,0 +1,20 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+//const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+
+button1.onclick = function () {
+  window.location.href = "index.html";
+};
+
+button2.onclick = function () {
+  window.location.href = "index.html";
+};
+
+//button3.onclick = function () {
+ // window.location.href = "index.html";
+//};
+
+button4.onclick = function () {
+  window.location.href = "aboutus.html";
+};
